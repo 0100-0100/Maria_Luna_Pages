af1fb76bf17e840c49554689c1cb7653e95d37fa
Diego <dfelo93@gmail.com>
2025-03-13 19:00:20 -0500

    ADDED | Navigation aside bar and styles.css

--- sapzurro/___/views/views.py
+++ sapzurro/___/views/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     context = {
-        'foreground_color': '#F8F6E0',
-        'background_color': '#275950'
+        'foreground_color': '#3BC2AB',
+        'background_color': '#1C4036'
     }
     return render(request, 'home.html', context)
 
@@ -12,6 +12,6 @@ def home(request):
 def map_view(request):
     context = {
         'foreground_color': '#275950',
-        'background_color': '#F8F6E0'
+        'background_color': '#1C4036'
     }
     return render(request, 'home.html', context)
