3904eb6958ef8f104c1c2bd577cdc6de0caa7caa
Diego <dfelo93@gmail.com>
2025-03-14 01:46:02 -0500

    ADDED | colors dict and map.html

--- sapzurro/___/views/views.py
+++ sapzurro/___/views/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
+colors = {
+    'white': '#FFF7D8',
+    'black': '#1C1C1C',
+    'main-1': '#1C4036',
+    'main-2': '#3BC2AB',
+    'main-3': '#6FAE43',
+    'yellow': '#F1B603'
+}
+
 
 # Create your views here.
 def home(request):
     context = {
-        'foreground_color': '#3BC2AB',
-        'background_color': '#1C4036'
+        'foreground_color': colors['main-1'],
+        'background_color': colors['white']
     }
     return render(request, 'home.html', context)
 
 
 def map_view(request):
     context = {
-        'foreground_color': '#275950',
-        'background_color': '#1C4036'
+        'foreground_color': colors['main-2'],
+        'background_color': colors['black']
     }
-    return render(request, 'home.html', context)
+    return render(request, 'map.html', context)
