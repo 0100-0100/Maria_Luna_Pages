3f42ede9a08dc83656da9a5d85d1b990e425fd7e
Diego <dfelo93@gmail.com>
2025-03-16 02:04:59 -0500

    ADDED | Dynamic image URL generation.

--- sapzurro/___/views/views.py
+++ sapzurro/___/views/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.conf import settings
+from django.templatetags.static import static
+import os
+
+
 colors = {
     'white': '#FFF7D8',
     'black': '#1C1C1C',
@@ -12,9 +17,19 @@ colors = {
 # Create your views here.
 def home(request):
     context = {
-        'foreground_color': colors['main-1'],
-        'background_color': colors['white']
+        'foreground_color': colors['yellow'],
+        'background_color': colors['main-1']
     }
+    carousel_path = 'carousel-1'
+    image_dir = os.path.join(settings.MEDIA_URL, carousel_path)
+    image_files = [
+        {'url': f'{settings.MEDIA_URL}{carousel_path}/{filename}'}
+        for filename in os.listdir(os.path.join(settings.MEDIA_ROOT, carousel_path))
+        if filename.endswith(('.jpg', '.jpeg', '.png', '.gif'))
+    ]
+    context.update({
+        'images': image_files
+    })
     return render(request, 'home.html', context)
 
 
