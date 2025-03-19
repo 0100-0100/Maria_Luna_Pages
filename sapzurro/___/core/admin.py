111a99e960922be852c490ce3f5a12601c11c303
Diego <dfelo93@gmail.com>
2025-03-19 01:51:53 -0500

    ADDED | ShouldKnowSection and Illustration models.

--- sapzurro/___/core/admin.py
+++ sapzurro/___/core/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from core.models import Photo, Carousel
+from core.models import Photo, Carousel, ShouldKnowSection, Illustration
 
 admin.site.register(Photo)
 admin.site.register(Carousel)
+admin.site.register(ShouldKnowSection)
+admin.site.register(Illustration)
