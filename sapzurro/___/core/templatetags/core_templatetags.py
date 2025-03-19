ee9dcc9828cec553cc4b9999464a2c031cad773d
Diego <dfelo93@gmail.com>
2025-03-19 01:53:28 -0500

    ADDED | get_know_about_sections inclusion tag.

--- sapzurro/___/core/templatetags/core_templatetags.py
+++ sapzurro/___/core/templatetags/core_templatetags.py
@@ -1,5 +1,5 @@
 from django import template
-from core.models import Carousel
+from core.models import Carousel, ShouldKnowSection
 
 register = template.Library()
 
@@ -8,4 +8,10 @@ register = template.Library()
 def get_carousel(carousel_name):
     return {
         'images': Carousel.objects.get(name=carousel_name).get_image_urls()
+    }
+
+@register.inclusion_tag("shared/know_about_section.html")
+def get_know_about_sections():
+    return {
+        'sections': ShouldKnowSection.objects.all()
     }
