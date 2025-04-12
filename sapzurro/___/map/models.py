3988b91bd771929e14e3e8b38dc08a8d1a275114
Diego <dfelo93@gmail.com>
2025-04-12 13:29:41 -0500

    ADDED | Additional fields for Location model.

--- sapzurro/___/map/models.py
+++ sapzurro/___/map/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from core.models import Photo
 
 class Location(models.Model):
-    name = models.CharField(max_length=255)
-    x = models.IntegerField(help_text="X-coordinate in pixels")
-    y = models.IntegerField(help_text="Y-coordinate in pixels")
-    icon = models.ImageField(upload_to='icons/', help_text="Icon for the location")
+    name = models.CharField(max_length=255, help_text="Name of the Location.", default="Location Name")
+    phone = models.CharField(null=True, max_length=255, help_text="Phone number for the Location.")
+    description = models.TextField(null=True, help_text="Long description of the Location.")
+    email = models.CharField(null=True, max_length=255, help_text="Email for the Location.")
+    instagram_link = models.CharField(null=True, max_length=255, help_text="Link for Instagram if available.")
+    website_link = models.CharField(null=True, max_length=255, help_text="Link for a website if available.")
+    icon = models.ManyToManyField(Photo, blank=True, related_name="map_location_marker", help_text="Icon for the location.")
+    x = models.IntegerField(null=True, help_text="X-coordinate in pixels.")
+    y = models.IntegerField(null=True, help_text="Y-coordinate in pixels.")
 
     def __str__(self):
         return self.name
