3f73787f85854016177ef29af809b64f2f43263a
Diego <dfelo93@gmail.com>
2025-04-20 17:32:54 -0500

    ADDED | get_icon_url SerializerMethodField.

--- sapzurro/___/map/serializers.py
+++ sapzurro/___/map/serializers.py
@@ -2,6 +2,15 @@ from rest_framework import serializers
 from .models import Location
 
 class LocationSerializer(serializers.ModelSerializer):
+    icon_url = serializers.SerializerMethodField('get_icon_url')
+
     class Meta:
         model = Location
-        fields = ['id', 'name', 'x', 'y', 'icon']
+        fields = ('id', 'name', 'x', 'y', 'icon_url')
+
+    def get_icon_url(self, location):
+        request = self.context.get('request')
+        if location.icon is not None:
+            icon_url = location.icon.image.url
+            return request.build_absolute_uri(icon_url)
+        return None
