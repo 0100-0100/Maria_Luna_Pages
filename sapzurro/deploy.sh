5ed7496e358dbc9776fd0ec3e123c3978a729288
Diego <dfelo93@gmail.com>
2025-04-25 19:57:58 -0500

    ADDED | Deployment script, run with cat deploy.sh | ssh sapzurro

---
+++ sapzurro/deploy.sh
@@ -0,0 +1,39 @@
+wait_with_message() {
+    local pid=$1
+    local message=$2
+
+    local spin='|/-\\'
+    local i=0
+    while kill -0 "$pid" 2>/dev/null; do
+        i=$(( (i + 1) % 4 ))
+        printf "\033[32m%c \033[0m%s\r" "${spin:$i:1}" "$message..."
+    done
+    wait "$pid" #2>/dev/null
+    sleep 0.5
+    echo "----------------------------------------"
+    printf "\033[92m✓ \033[0m%s\n\n" "$message.       "
+}
+
+cd Maria_Luna_Pages/
+
+git pull origin main &
+wait_with_message $! "Pulling from main"
+
+source .venv/bin/activate
+cd sapzurro/___/
+
+python3 manage.py collectstatic --noinput &
+wait_with_message $! "Collecting Static"
+
+python3 manage.py makemigrations &
+wait_with_message $! "Making migrations"
+
+python3 manage.py migrate &
+wait_with_message $! "Migrating"
+
+sudo service sapzurro restart &
+wait_with_message $! "Restarting sapzurro service"
+
+
+echo "----------------------------------------"
+echo -e "\033[92m✓ \033[0mSuccessfully Updated Sapzurro Page."
