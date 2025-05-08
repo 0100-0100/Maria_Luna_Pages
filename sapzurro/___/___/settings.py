032e275f2fd8f7eb8a30d1363602c5f82782823d
Diego <dfelo93@gmail.com>
2025-05-08 00:27:32 -0500

    UPDATED | STATIC_ROOT to absolute path.

--- sapzurro/___/___/settings.py
+++ sapzurro/___/___/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / '/var/www/sapzurro/static/' if not DEBUG else None
+STATIC_ROOT = '/var/www/sapzurro/static/' if not DEBUG else None
 STATICFILES_DIRS = (BASE_DIR / 'static/',)
 # if not DEBUG:
 #     STATIC_ROOT = BASE_DIR / '/var/www/sapzurro/static/'
