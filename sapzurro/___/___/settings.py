e5096d59a9ddcfac7af9a202d936dd31afeb92a8
Diego <dfelo93@gmail.com>
2025-05-07 21:57:40 -0500

    ADDED | Ternary conditions instead of if statements.

--- sapzurro/___/___/settings.py
+++ sapzurro/___/___/settings.py
@@ -12,14 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from os import getenv as env
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-
 DEBUG = env('DJANGO_DEBUG')
 
 if not DEBUG:
@@ -34,20 +32,13 @@ ALLOWED_HOSTS = [
     'mapasapzurro.co',
     'www.mapasapzurro.co'
 ]
-if DEBUG:
-    ALLOWED_HOSTS += [
-        'localhost',
-        '127.0.0.1',
-    ]
+ALLOWED_HOSTS += ['localhost', '127.0.0.1'] if DEBUG else []
 
 CORS_ALLOWED_ORIGINS = [
     'https://www.mapasapzurro.co',
     'https://mapasapzurro.co'
 ]
-if DEBUG:
-    CORS_ALLOWED_ORIGINS += [
-        'http://localhost',
-    ]
+CORS_ALLOWED_ORIGINS += ['http://localhost'] if DEBUG else []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -78,28 +69,25 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = '___.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
+TEMPLATES = [{
+    'BACKEND': 'django.template.backends.django.DjangoTemplates',
+    'DIRS': ['templates'],
+    'APP_DIRS': True,
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
     },
-]
+},]
 
 WSGI_APPLICATION = '___.wsgi.application'
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -110,48 +98,42 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+AUTH = 'django.contrib.auth.password_validation'
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': f'{AUTH}.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': f'{AUTH}.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': f'{AUTH}.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': f'{AUTH}.NumericPasswordValidator',
     },
 ]
 
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
-if not DEBUG:
-    STATIC_ROOT = BASE_DIR / '/var/www/sapzurro/static/'
-STATICFILES_DIRS = (
-    BASE_DIR / 'static/',
-)
-
+STATIC_ROOT = BASE_DIR / '/var/www/sapzurro/static/' if not DEBUG else None
+STATICFILES_DIRS = (BASE_DIR / 'static/',)
+# if not DEBUG:
+#     STATIC_ROOT = BASE_DIR / '/var/www/sapzurro/static/'
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media/"
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
