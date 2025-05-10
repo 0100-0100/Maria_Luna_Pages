7dca793def5d7ed8984a46b48cc3144f5762b41e
Diego <dfelo93@gmail.com>
2025-05-10 00:05:35 -0500

    UPDATED | Bounds and zoom levels.

--- sapzurro/___/static/js/map/setup.js
+++ sapzurro/___/static/js/map/setup.js
@@ -1,64 +1,63 @@
 const w = 8192;
 const h = 5851;
-const legend = document.getElementById('legend');
-const legendButton = document.getElementById('legend-toggle');
-legendButton.addEventListener('click', toggleLegend);
 
 function toggleLegend() {
   legend.classList.toggle('visible');
   legendButton.innerText = legendButton.innerText === 'Show Legend' ? 'Hide Legend' : 'Show Legend';
 }
+const legend = document.getElementById('legend');
+const legendButton = document.getElementById('legend-toggle');
+legendButton.addEventListener('click', toggleLegend);
 
 const map = L.map('map', {
   crs: L.CRS.Simple,
-  minZoom: 4,
-  maxZoom: 7,
+  minZoom: 0,
+  maxZoom: 4,
   zoomControl: false,
   attributionControl: false,
-  zoomSnap: 0
- }).setView([-32, 32], 4);
+  zoomSnap: 0.01
+ }).setView([-32, 32], 0);
 
 L.control.zoom({ position: 'bottomleft' }).addTo(map);
-const southWest = map.unproject([0, h], map.getMaxZoom());
-northEast = map.unproject([w, 0], map.getMaxZoom());
-const bounds = L.latLngBounds(southWest, northEast);
+let southWest = map.unproject([0, h - 90], map.getMaxZoom());
+let northEast = map.unproject([w - 128, 0], map.getMaxZoom());
+let bounds = L.latLngBounds(southWest, northEast);
+map.setMaxBounds(bounds);
 
-L.tileLayer('/static/map/tiles/{z}-{x}-{y}.webp', {
+L.tileLayer('/static/map/tiles_files/{z}/{x}_{y}.webp', {
   tileSize: 96,
-  minZoom: 4,
-  maxZoom: 7,
-  zoomOffset: 0,
+  minZoom: 0,
+  maxZoom: 4,
+  zoomOffset: 3,
   center: [-32, 32],
   noWrap: true,
   tms: false,
   bounds: bounds,
   unloadInvisibleTiles: false,
   updateWhenIdle: false,
-  keepBuffer: 1000
+  keepBuffer: 10000
 }).addTo(map);
-
-map.setMaxBounds(bounds);
 
 const markers = [];
 fetch('/api/locations/')
   .then(response => response.json())
   .then(data => {
-    data.forEach(location => {
-      const marker = L.marker(
-        [location.y, location.x],
-        {
-          icon: L.icon({
-            iconUrl: location.icon_url ? location.icon_url : '/static/images/question-sign.webp',
-            iconSize: [32, 32],
-            className: 'map-marker-0'
-          })
-        }
-      ).addTo(map);
-      marker.bindPopup("<b>" + location.name + "</b>");
-      markers.push({ name: location.name, marker: marker });
-
-      const li = getMarkerLegendTemplate(location, marker);
-      document.getElementById('legend-list').appendChild(li);
+    data.forEach(
+      location => {
+        const marker = L.marker(
+          [location.y, location.x],
+          {
+            icon: L.icon({
+              iconUrl: location.icon_url ? location.icon_url : '/static/images/question-sign.webp',
+              iconSize: [32, 32],
+              className: 'map-marker-0'
+            })
+          }
+        ).addTo(map);
+        marker.bindPopup("<b>" + location.name + "</b>");
+        markers.push({ name: location.name, marker: marker });
+        const li = getMarkerLegendTemplate(location, marker);
+        document.getElementById('legend-list').appendChild(li);
     });
   });
 
