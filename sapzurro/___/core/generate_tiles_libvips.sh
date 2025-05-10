1ab37d261e9fe55e287bf00f4c183ba4fb065922
Diego <dfelo93@gmail.com>
2025-05-10 00:34:50 -0500

    ADDED | rm of temporary files and correct structure of tiles.

--- sapzurro/___/core/generate_tiles_libvips.sh
+++ sapzurro/___/core/generate_tiles_libvips.sh
@@ -2,9 +2,9 @@
 
 # Default values
 INPUT_IMAGE=${1:-"core/Sin logos ni leyenda.png"}
-OUTPUT_DIR=${2:-"static/map/tiles"}
-#TILE_SIZE=${3:-96}
-TILE_SIZE=8051
+OUTPUT_DIR=${2:-"static/map/tiles/"}
+TILE_SIZE=${3:-96}
+#TILE_SIZE=8051
 echo "Generating tiles for: $INPUT_IMAGE"
 echo "Output directory: $OUTPUT_DIR"
 echo "Tile size: $TILE_SIZE"
@@ -14,7 +14,7 @@ rm -rf "$OUTPUT_DIR"
 mkdir -p "$OUTPUT_DIR"
 
 # Generate DeepZoom tiles using vips
-vips dzsave "$INPUT_IMAGE" "$OUTPUT_DIR" \
+vips dzsave "$INPUT_IMAGE" static/map/tiles \
   --tile-size=$TILE_SIZE \
   --overlap=0 \
   --suffix=".webp[Q=100]" \
@@ -22,3 +22,8 @@ vips dzsave "$INPUT_IMAGE" "$OUTPUT_DIR" \
   --layout=dz \
   --centre \
   --strip
+
+rm static/map/tiles.dzi
+rm static/map/tiles_files/vips-properties.xml
+mv static/map/tiles_files/* static/map/tiles
+rm -rf static/map/tiles_files
