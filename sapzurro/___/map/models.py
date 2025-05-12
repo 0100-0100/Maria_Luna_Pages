5813f5aa3531ba9e80f9801ae7e5afc63b189b74
Diego <dfelo93@gmail.com>
2025-05-12 04:37:10 -0500

    UPDATED | Location model.

--- sapzurro/___/map/models.py
+++ sapzurro/___/map/models.py
@@ -1,24 +1,60 @@
+"""Map Models module."""
 from django.db import models
+
+location_field_base_settings = {
+    'null': True, 'blank': True, 'default': None, 'max_length': 255
+}
 
 
 class MapLogoIcon(models.Model):
     """Icon model."""
+
     name = models.CharField(max_length=255)
     image = models.ImageField(upload_to='map_logos/')
     added = models.DateField(auto_now_add=True)
 
     def __str__(self):
+        """__str__ dunder method."""
         return f"{self.name} | {self.added}"
 
+
 class Location(models.Model):
-    name = models.CharField(max_length=255, help_text="Name of the Location.", default=None)
-    phone = models.CharField(null=True, blank=True, default=None, max_length=255, help_text="Phone number for the Location.")
-    description = models.TextField(null=True, blank=True, default=None, help_text="Long description of the Location.")
-    email = models.CharField(null=True, blank=True, default=None, max_length=255, help_text="Email for the Location.")
-    instagram_link = models.CharField(null=True, blank=True, default=None, max_length=255, help_text="Link for Instagram if available.")
-    website_link = models.CharField(null=True, blank=True, default=None, max_length=255, help_text="Link for a website if available.")
-    x = models.FloatField(null=True, help_text="X-coordinate in pixels.", default=0)
-    y = models.FloatField(null=True, help_text="Y-coordinate in pixels.", default=0)
+    """Location | Represents a marker location on the map."""
+
+    name = models.CharField(
+        **location_field_base_settings,
+        help_text="Name of the Location."
+    )
+    description = models.TextField(
+        **location_field_base_settings,
+        help_text="Long description of the Location."
+    )
+    phone = models.CharField(
+        **location_field_base_settings,
+        help_text="Phone number for the Location."
+    )
+    instagram_link = models.CharField(
+        **location_field_base_settings,
+        help_text="Link for Instagram if available."
+    )
+    facebook_link = models.CharField(
+        **location_field_base_settings,
+        help_text="Link for Facebook if available."
+    )
+    website_link = models.CharField(
+        **location_field_base_settings,
+        help_text="Link for a website if available."
+    )
+    email = models.CharField(
+        **location_field_base_settings,
+        help_text="Email for the Location."
+    )
+    x = models.FloatField(
+        null=True, help_text="X-coordinate in pixels.", default=0
+    )
+    y = models.FloatField(
+        null=True, help_text="Y-coordinate in pixels.", default=0
+    )
     icon = models.OneToOneField(
         MapLogoIcon,
         on_delete=models.CASCADE,
@@ -30,4 +66,5 @@ class Location(models.Model):
     )
 
     def __str__(self):
+        """__str__ dunder method."""
         return self.name
