f010b7a6798b3b457029276f225042d91989a10a
Diego <dfelo93@gmail.com>
2025-05-16 01:57:02 -0500

    ADDED | Initialization of description carousels.

--- sapzurro/___/static/js/carousel.js
+++ sapzurro/___/static/js/carousel.js
@@ -1,6 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   var images = document.querySelectorAll(".carousel-image");
+  var imagesDescription = document.querySelectorAll(".carousel-description-image");
+  images = [...images, ...imagesDescription];
   var swiperInitialized = false;
+  var swiperDescriptionInitialized = false;
 
   function initializeSwiper() {
     if (!swiperInitialized) {
@@ -25,6 +28,29 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   }
 
+  function initializeSwiperDescription() {
+    if (!swiperDescriptionInitialized) {
+      swiperDescriptionInitialized = true;
+      new Swiper(".swiper-container-description", {
+        slidesPerView: 1,
+        centeredSlides: true,
+        loop: true,
+          navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        autoplay: {
+          delay: 15000,
+          disableOnInteraction: true,
+        },
+      });
+    }
+  }
+
   images.forEach((img) => {
     const imgPreload = new Image();
     const imgUrl = img.dataset.src;
@@ -35,6 +61,7 @@ document.addEventListener("DOMContentLoaded", function () {
           response.blob().then((blob) => {
             img.src = URL.createObjectURL(blob); // Load from cache
             initializeSwiper();
+            initializeSwiperDescription();
           });
         } else {
           imgPreload.src = imgUrl;
@@ -42,6 +69,7 @@ document.addEventListener("DOMContentLoaded", function () {
             img.src = imgPreload.src;
             cache.add(imgUrl); // Cache for future use
             initializeSwiper();
+            initializeSwiperDescription();
           };
         }
       });
