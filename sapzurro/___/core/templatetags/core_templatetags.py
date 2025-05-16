8004f8151a487bbc9dddf24217616eb4c174cd45
Diego <dfelo93@gmail.com>
2025-05-16 01:54:07 -0500

    UPDATED | Renamed get_description_and_image_urls -> get_context_fields

--- sapzurro/___/core/templatetags/core_templatetags.py
+++ sapzurro/___/core/templatetags/core_templatetags.py
@@ -8,7 +8,7 @@ register = template.Library()
 def get_carousel(carousel_name):
     carousel = Carousel.objects.get(name=carousel_name)
     return {
-        'images': carousel.get_description_and_image_urls(),
+        'images': carousel.get_context_fields(),
         'show_description': carousel.show_description,
     }
 
